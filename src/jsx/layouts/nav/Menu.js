782e7c3cb23f8bd0227f89e0006d3631e8a571ba
Barto <juanmartin.cerruti@gmail.com>
2023-10-10 15:08:02 -0300

    Revert "first commit branch, eliminada seccion apps"
    This reverts commit 841c12877f0a8c9a93924527c6c04f156f23582d.

--- src/jsx/layouts/nav/Menu.js
+++ src/jsx/layouts/nav/Menu.js
@@ -39,7 +39,85 @@ export const MenuList = [
             },			
         ],
     },
-
+    
+    //Apps
+    {
+        title: 'Apps',	
+        classsChange: 'mm-collapse',
+        iconStyle: <i className="fas fa-info-circle"></i>,
+        content: [
+            {
+                title: 'Profile',
+                to: 'app-profile'
+            },
+            // {
+            //     title: 'Edit Profile',
+            //     to: 'edit-profile'
+            // },
+            {
+                title: 'Post Details',
+                to: 'post-details'
+            },
+            {
+                title: 'Email',
+                //to: './',
+                hasMenu : true,
+                content: [
+                    {
+                        title: 'Compose',
+                        to: 'email-compose',
+                    },
+                    {
+                        title: 'Index',
+                        to: 'email-inbox',
+                    },
+                    {
+                        title: 'Read',
+                        to: 'email-read',
+                    }
+                ],
+            },
+            {
+                title:'Calendar',
+                to: 'app-calender'
+            },
+            {
+                title: 'Shop',
+                //to: './',
+                hasMenu : true,
+                content: [
+                    {
+                        title: 'Product Grid',
+                        to: 'ecom-product-grid',
+                    },
+                    {
+                        title: 'Product List',
+                        to: 'ecom-product-list',
+                    },
+                    {
+                        title: 'Product Details',
+                        to: 'ecom-product-detail',
+                    },
+                    {
+                        title: 'Order',
+                        to: 'ecom-product-order',
+                    },
+                    {
+                        title: 'Checkout',
+                        to: 'ecom-checkout',
+                    },
+                    {
+                        title: 'Invoice',
+                        to: 'ecom-invoice',
+                    },
+                    {
+                        title: 'Customers',
+                        to: 'ecom-customers',
+                    },
+                ],
+            },
+        ],
+    },
     //Charts
     {
         title: 'Charts',	
