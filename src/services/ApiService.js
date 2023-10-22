aec51c7bbbdeff198578451c136d8c779e3258d7
Barto <juanmartin.cerruti@gmail.com>
2023-10-22 19:04:59 -0300

    make proxy works

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -2,17 +2,30 @@ import axios from 'axios';
 
 const apiMockURL = `/mock/`
 const apiURL = `https://gesto-api.vercel.app/api/v1`
+const proxyUrl = "https://my-proxy-henna.vercel.app/api/proxy"
+// const proxyUrl = "http://localhost:3001/api/proxy"
 
 class ApiService {
     axios;
     constructor() {
         this.axios = axios.create({
             baseURL: apiURL,
+            withCredentials: false,
         });
     }
 
     doLogin(email, password) {
-        return this.axios.post(`/auth/login`, { email, password });
+        
+        return this.axios.post(proxyUrl, {
+            "method": "post",
+            "url": apiURL + "/auth/login",
+            "data": {
+                email,
+                password
+            }
+        });
+        
+        //return this.axios.post(`/auth/login`, { email, password });
     }
 
     getRooms() {
